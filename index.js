e5ce2aee044bbecac996a7b80b45ffcf7b7dbd09
madelinemc <madeline.coons@gmail.com>
2021-01-16 21:32:32 -0600

    commit tree 0c080795b172f7d102106c4559083bc08bf60907

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+//submit data should return the fetch()
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name, email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        document.body.innerHTML = data['id']
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access")
+        document.body.innerHTML = error.message
+    })
+}
